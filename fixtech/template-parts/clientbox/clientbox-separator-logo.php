b74a46e96b6fb1b4de900314e9abfc51377fbd27
Jamshid Khaksaar <jamshid.khaksaar@outlook.com>
2026-01-18 15:10:07 +0430

    chore: remove old WordPress theme and database files
    Removed legacy files from previous WordPress project:
    - fixtech/ (WordPress theme)
    - db_data/ (MySQL database files)
    - conductor/ (project management)
    - docker-compose.yml, db_dump.sql
    - .gemini-clipboard/

--- fixtech/template-parts/clientbox/clientbox-separator-logo.php
+++
@@ -1,3 +0,0 @@
-<span class="themetechmount-box themetechmount-box-client themetechmount-box-view-separator-logo themetechmount-client-box-view-separator-logo">
-	<?php echo themetechmount_wp_kses(themetechmount_featured_image()); ?>
-</span>
--- fixtech/template-parts/clientbox/clientbox-simple-logo.php
+++
@@ -1,3 +0,0 @@
-<span class="themetechmount-box themetechmount-box-client themetechmount-box-view-simple-logo themetechmount-client-box-view-simple-logo">
-	<?php echo themetechmount_wp_kses(themetechmount_featured_image()); ?>
-</span>
